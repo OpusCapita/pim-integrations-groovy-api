47f7e097a0f1f6e3cf8bfd246f1b02fad0766417
Brüseke Dennis <dennis.brueseke@opuscapita.com>
2018-10-22 14:24:08 +0200

    Add Method: getInstance

--- customizationArea/server/serviceMethods/ping.groovy
+++ customizationArea/server/serviceMethods/ping.groovy
@@ -1,13 +1,33 @@
+import org.apache.commons.logging.Log
+import org.apache.commons.logging.LogFactory
 
 /**
-	Groovydoc test - Class
+* Initializes an Instance of the Customizationservice
+* @author  Dennis Brueseke
+* @version 1.0
 */
-class Ping{
-	/**
-		Groovydoc test - Method
-	*/
-	def execute(){
+class getInstance{
 
-	}
+    /**
+     * [execute description]
+     * @return [description]
+     */
+    Customizationservice execute(){
+        new CustomizationService()
+    }
 
+}
+
+/**
+ * Provides methods for customization tasks
+ */
+class CustomizationService{
+    Log log = LogFactory.getLog(this.class)
+
+    /**
+     * Ping the PIT
+     */
+    public static String ping(){
+        "Some msg"
+    }
 }
