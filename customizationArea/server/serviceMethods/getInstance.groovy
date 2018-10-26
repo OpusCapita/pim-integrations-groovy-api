935706cad4dba0f704c2787ace023561fb2ab995
Brüseke Dennis <dennis.brueseke@opuscapita.com>
2018-10-26 10:07:07 +0200

    Add URL

--- customizationArea/server/serviceMethods/getInstance.groovy
+++ customizationArea/server/serviceMethods/getInstance.groovy
@@ -1,6 +1,3 @@
-import java.net.ConnectException
-import groovy.json.JsonSlurper
-import groovy.json.JsonParserType
 
 class GetInstance{
 
@@ -9,61 +6,40 @@ class GetInstance{
      * @return [description]
      */
     CustomizationService execute(String host, int port, String accessToken){
-        new CustomizationService(host,port,accessToken)
+        new CustomizationService(host, port, accessToken)
     }
 }
 
 /**
  * Provides methods for customization tasks
- * @author  Dennis Brueseke
- * @version 1.0
  */
 class CustomizationService{
 
-    private String host, accessToken
-    private int port
-    private final static String apiPath = 'api'
+    private URL url
+    private final static basePath = '/api/'
 
     /**
      * Creates CustomizationService from the host, port number and access token
+     * @param host        Host url
+     * @param port        Port
+     * @param accessToken Valid access token
      */
-    public CustomizationService(String host, int port, String accessToken) throws ConnectException{
-        boolean pitIsAvailable
-        try{
-            pitIsAvailable = ping(host,port,accessToken)
-        }catch (Exception e){
-            pitIsAvailable = false
-            throw e
-        }
-
-        if(pitIsAvailable){
-            this.host = host
-            this.port = port
-            this.accessToken = accessToken
-        }else{
-            throw new ConnectException ("Connection refused. Check host, port and accessToken for validity")
-        }
+    public CustomizationService(String host, int port, String accessToken){
+        url = host.toURL
+        url.port = port
+        url.path = basePath
+        url.query = 'token='+accessToken
     }
 
     /**
      * Ping the PIT
-     * @param  host        Host URL where the PIT is running
-     * @param  port        Port
-     * @param  accessToken GeneratedPIT access token
-     * @return             Connection is possible true/false
+     * @return             Ping response
      */
-    private static boolean ping(String host, int port, String accessToken){
-        URL url = "${host}:${port}/${apiPath}/ping/?token=${accessToken}".toString().toURL()
-
-        def response = url.getText()
-        response = parseJSON(response)
-
-        int status = response.status
-        status in [200]
+    private static boolean ping(){
+        url.path = basePath + 'ping/'
+        String response = url.getText()
+        url.path = basePath
+        response
     }
 
-    private static def parseJSON(String jsonText){
-        def parser = new JsonSlurper().setType(JsonParserType.LAX)
-        parser.parseText(jsonText)
-    }
 }
