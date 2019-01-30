f2156b357c73fbf7fb1c23f48c53755083ff1e72
davidbrien <david.brien@opuscapita.com>
2019-01-30 08:37:15 +0100

    FEAT: add supplier endpoint

--- customizationArea/server/serviceMethods/pimApi.groovy
+++ customizationArea/server/serviceMethods/pimApi.groovy
@@ -64,6 +64,9 @@ class PitGroovyApi {
 
     private Closure generalContractsPath = { -> "/api/contract"}
     private Closure contractPath = {contractId -> "${generalContractsPath()}/$contractId"}
+    
+    private Closure generalSuppliersPath = { -> "/api/supplier"}
+    private Closure supplierPath = {supplierId -> "${generalSuppliersPath()}/$supplierId"}
 
     /**
      * Creates a new API object with the given url and access token.
@@ -521,6 +524,39 @@ class PitGroovyApi {
         String path = contractPath(contractId)
         restGet(path)
     }
+/**
+     * Retrieve a Supplier
+     * @param  supplierId supplierId
+     * @return supplier
+     * @throws NotAuthorizedException
+     * @throws UnknownHostException
+     * @throws GroovyAPIInternalErrorException
+     * @throws PITInternalErrorException
+     * @throws PIMAccessDeniedException
+     * @throws PIMUnreachableException
+     * @throws PIMInternalErrorException
+     */
+
+    public Response getSupplier(String supplierId) {
+        String path = supplierPath(supplierId)
+        restGet(path)
+    }
+    /**
+     * Retrieve all Suppliers
+     * @return List of all Suppliers
+     * @throws NotAuthorizedException
+     * @throws UnknownHostException
+     * @throws GroovyAPIInternalErrorException
+     * @throws PITInternalErrorException
+     * @throws PIMAccessDeniedException
+     * @throws PIMUnreachableException
+     * @throws PIMInternalErrorException
+     */
+    public Response getAllSuppliers() {
+        String path = generalSuppliersPath()
+        restGet(path)
+    }
+
 
     private Response restGet(String path, LinkedHashMap query = [:]) {
         def response
