35bf5f5060643a3796c7b4353e35ef0021e2e49e
davidbrien <david.brien@opuscapita.com>
2019-03-06 17:02:26 +0100

    fix : url encoding

--- customizationArea/server/serviceMethods/pimApi.groovy
+++ customizationArea/server/serviceMethods/pimApi.groovy
@@ -2,6 +2,7 @@ import org.apache.commons.validator.routines.UrlValidator
 import javax.ws.rs.InternalServerErrorException
 import groovyx.net.http.HttpResponseException
 import groovyx.net.http.RESTClient
+import groovyx.net.http.URIBuilder
 import org.apache.http.entity.ContentType
 import org.apache.http.conn.ConnectTimeoutException
 import org.apache.http.conn.HttpHostConnectException
@@ -741,18 +742,21 @@ class PitGroovyApi {
      */
 
     public Response getDocument(String documentId) {
-        String path = documentPath(documentId)
-        restGet(path)
+        def encodedDocumentId = URLEncoder.encode(documentId,'UTF-8')
+        String path = documentPath(encodedDocumentId)
+        restGet(path,[meta:true])
     }
     
     private Response restGet(String path, LinkedHashMap query = [:]) {
         def response
         query.put('token', accessToken)
+        def uri = new URIBuilder(new URI(restClient.uri.toString() + path))
         try {
-            response = restClient.get([path: path,
+            response = restClient.get([uri: uri,
                 contentType: ContentType.APPLICATION_JSON,
                 query: query
             ])
+
         } catch (Exception e) {
             if(e instanceof HttpResponseException && e.response.data.status == 404){
                 return new Response([])
